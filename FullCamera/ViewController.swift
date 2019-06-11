886f178e2421a78641c2108edca9e0ced47429a0
dp22125 <dp221125@naver.com>
2019-06-11 16:45:51 +0900

    [Close #1] Remove StoryBoard

---
+++ FullCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FullCamera
+//
+//  Created by Meo MacBook Pro on 11/06/2019.
+//  Copyright © 2019 Meo MacBook Pro. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
